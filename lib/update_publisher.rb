21d12fe0c04280f50a6e893174c07b0cb7872d7e
Kyle McKee <mckee.kyle@gmail.com>
2014-10-26 17:12:22 -0400

    Micro commit.

--- lib/update_publisher.rb
+++ lib/update_publisher.rb
@@ -2,6 +2,6 @@ require 'httparty'
 
 class UpdatePublisher
   def self.update(widget, values)
-    HTTParty.post("some_url", {:key => "value"}.to_json)
+    HTTParty.post('http://localhost:3030/', {:key => "value"}.to_json)
   end
 end
--- spec/update_publisher_spec.rb
+++ spec/update_publisher_spec.rb
@@ -4,6 +4,11 @@ describe UpdatePublisher do
   it "sends a http post to a url" do
     allow(HTTParty).to receive(:post)
     UpdatePublisher.update(:karma, {:current => 100})
-    expect(HTTParty).to have_received(:post)
+    expect(HTTParty).to have_received(:post).with(any_args)
+  end
+  it "posts to localhost:3030 by default" do
+    allow(HTTParty).to receive(:post)
+    UpdatePublisher.update(:karma, {:current => 100})
+    expect(HTTParty).to have_received(:post).with(/http:\/\/localhost:3030\//, anything)
   end
 end
