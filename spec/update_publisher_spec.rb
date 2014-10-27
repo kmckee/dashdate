efdd40f1386c16a55b5d9af9ce56894787ab8c7f
Kyle McKee <mckee.kyle@gmail.com>
2014-10-26 22:51:26 -0400

    Micro commit.

--- spec/update_publisher_spec.rb
+++ spec/update_publisher_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 require 'httparty'
 describe UpdatePublisher do
-  it "sends a http post to a url" do
-    allow(HTTParty).to receive(:post)
-    UpdatePublisher.update(:karma, {:current => 100})
-    expect(HTTParty).to have_received(:post).with(any_args)
-  end
-  it "posts to localhost:3030 by default" do
-    allow(HTTParty).to receive(:post)
-    UpdatePublisher.update(:karma, {:current => 100})
-    expect(HTTParty).to have_received(:post).with(/http:\/\/localhost:3030\//, anything)
+  before(:each) { allow(HTTParty).to receive(:post) }
+  describe "#update" do
+    it "sends a http post to a url" do
+      UpdatePublisher.update(:karma, {:current => 100})
+      expect(HTTParty).to have_received(:post).with(any_args)
+    end
+    it "posts to localhost:3030 by default" do
+      UpdatePublisher.update(:karma, {:current => 100})
+      expect(HTTParty).to have_received(:post).with(/http:\/\/localhost:3030\//, anything)
+    end
   end
 end
