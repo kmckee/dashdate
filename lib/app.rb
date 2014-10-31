1bcb093915ee10a0c4d8202a6b046f0d87017abf
Kyle McKee <mckee.kyle@gmail.com>
2014-10-31 18:58:41 -0400

    Micro commit.

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,15 @@ module Dashdate
   class App
     attr_accessor :publisher
     def self.build
-      new(Dashdate::Publisher.new)
+      new(Dashdate::Publisher.new, nil)
     end
-    def initialize publisher
+    def initialize publisher, arg_parser
       @publisher = publisher
+      @arg_parser = arg_parser
     end
     def handle_args args
-      @publisher.update(:karma, nil)
+      parsed_args = @arg_parser.parse(args)
+      @publisher.update(parsed_args[:widget], parsed_args[:values])
     end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,28 +6,41 @@ describe Dashdate::App do
       app = Dashdate::App.build()
       app.publisher.should_not be_nil
     end
+    it 'creates an instance of an arg_parser' 
   end
   describe '#initialize' do
     it 'assigns the publisher' do
       publisher = double('publisher')
-      app = Dashdate::App.new(publisher)
+      app = Dashdate::App.new(publisher, nil)
       app.publisher.should == publisher
     end
   end
-  describe '#execute_command' do
+  describe '#handle_args' do
     let (:publisher) { double('publisher', :update => nil) }
-    subject { Dashdate::App.new(publisher) }
+    let (:arg_parser) { double('arg_parser', :parse => {}) }
+    subject { Dashdate::App.new(publisher, arg_parser) }
+
+    it 'sends the arguments into the parser' do 
+      expected_args = 'dashdate --widget karma --values valuation=100'
+      subject.handle_args(expected_args)
+      expect(arg_parser).to have_received(:parse).with(expected_args)
+    end
     it 'publishes to the correct widget' do
-      subject.handle_args(make_into_args('dashdate --widget karma'))
+      allow(arg_parser).to receive(:parse).and_return({ :widget => :karma })
+      subject.handle_args('')
       expect(publisher).to have_received(:update).with(:karma, anything)
     end
     it 'publishes to other widgets' do
-      subject.handle_args(make_into_args('dashdate --widget valuation'))
+      allow(arg_parser).to receive(:parse).and_return({ :widget => :valuation })
+      subject.handle_args('')
       expect(publisher).to have_received(:update).with(:valuation, anything)
+    end
+    it 'passes the values to the publisher' do
+      expected_values = { :value => 100 }
+      allow(arg_parser).to receive(:parse).and_return({ :values => expected_values })
+      subject.handle_args('')
+      expect(publisher).to have_received(:update).with(anything, expected_values)
     end
   end
 end
 
-def make_into_args string
-  string.split
-end
