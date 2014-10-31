52e4caf594e8016c422e21dc19c535c0d43b84f2
Kyle McKee <mckee.kyle@gmail.com>
2014-10-31 07:57:45 -0400

    First pass at a cuke

--- features/step_definitions/command_line_steps.rb
+++ features/step_definitions/command_line_steps.rb
@@ -1,8 +1,11 @@
-When(/^I enter the following in a terminal: "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I enter the following in a terminal: "(.*?)"$/) do |user_input|
+  words = user_input.split
+  words.shift.should == 'dashdate'
+  @publisher = double("publisher"
+  Dashdate::App.handle_command(words, publisher)
 end
 
-Then(/^the Karma widget should be updated with the following values:$/) do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+Then(/^the (.*) widget should be updated with the following values:$/) do |widget_name, expected_values_table|
+  expected_values = expected_values_table.hashes.first
+  expect(@publisher).to have_received(:post).with(widget_name.to_sym, expected_values)
 end
