852c8838ff1cd0eae7fa4a37903600b6bd2d6cc0
Kyle McKee <mckee.kyle@gmail.com>
2014-11-01 10:10:14 -0400

    Micro commit.

--- lib/arg_parser.rb
+++ lib/arg_parser.rb
@@ -1,13 +1,29 @@
-require 'optparse'
+require 'trollop'
 
 module Dashdate 
   class ArgParser
     def parse args
       args = args.split.drop(1) unless args.class == Array 
+      opts = parse_arg_array args 
       {
-        :widget => args[1].to_sym 
+        :widget => opts[:widget].to_sym,
+        :values => parse_value_string(opts[:values])
       }
-
     end
+    def parse_arg_array args
+      Trollop::options(args) do 
+        opt :widget, "The name of the widget to publish to", :required => true, :short => :w, :type => String
+        opt :values, "The values to push to the widget.  Format is: key1=value1,key2=value2", :required => true, :type => String
+      end
+    end
+    def parse_value_string string
+      values = {}
+      string.split(',').each do |key_equals_value|
+        key_value_pair = key_equals_value.split('=')
+        values[key_value_pair.first.to_sym] = key_value_pair.last
+      end
+      values
+    end
+
   end
 end
--- spec/arg_parser_spec.rb
+++ spec/arg_parser_spec.rb
@@ -3,15 +3,23 @@ require 'spec_helper'
 describe Dashdate::ArgParser do
   subject { Dashdate::ArgParser.new }
   it 'parses the long form of a widget name' do
-    subject.parse('dashdate --widget karma')[:widget].should == :karma  
+    subject.parse('dashdate --widget karma --values foo=1')[:widget].should == :karma  
   end
   it 'parses the short form of a widget name' do
-    subject.parse('dashdate -w valuation')[:widget].should == :valuation
+    subject.parse('dashdate -w valuation --values foo=1')[:widget].should == :valuation
   end
   it 'parses the widget name regardless of location' do
     subject.parse('dashdate --values foo=1 --widget karma')[:widget].should == :karma  
   end
-  it 'parses the values into a hash'
-  it 'sets the keys in the values hash'
-  it 'sets the values in the values hash'
+  it 'parses the values into a hash' do
+    subject.parse('dashdate -w karma --values foo=1')[:values].should be_kind_of(Hash)
+  end
+  it 'sets the keys in the values hash' do
+    values = subject.parse('dashdate -w karma --values foo=1')[:values]
+    values.keys.should include :foo
+  end
+  it 'sets the values in the values hash' do
+    values = subject.parse('dashdate -w karma --values foo=1')[:values]
+    values[:foo].should == '1'
+  end
 end
