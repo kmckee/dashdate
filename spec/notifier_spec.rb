fb3d1b6663540a4a8446aa4441f03337a61bf3e3
Kyle McKee <mckee.kyle@gmail.com>
2014-11-02 10:21:54 -0500

    Micro commit.

--- spec/notifier_spec.rb
+++ spec/notifier_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
 describe Dashdate::Notifier do
+  let(:writer) { double('writer', :write => nil) }
+  subject { Dashdate::Notifier.new(writer) }
   it 'notifies success when 204 is received' do
-    writer = double('writer', :write => nil)
-    subject = Dashdate::Notifier.new(writer)
     subject.received_http_response_code(204)
     expect(writer).to have_received(:write).with('Successfully updated dashboard!')
   end
   it 'notifies an authentication problem when a 401 is received.' do
-    writer = double('writer', :write => nil)
-    subject = Dashdate::Notifier.new(writer)
     subject.received_http_response_code(401)
     expect(writer).to have_received(:write).with('Error: Invalid Authentication Token.  Check your config.ru file.')
   end
